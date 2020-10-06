4edbbf45822287879a5f09d5696fd692b89342eb
shamim018455 <istiakmahamud780@gmail.com>
2020-10-06 22:59:21 +0600

    Ecommerce

---
+++ app/src/main/java/com/example/ecommerceclone/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.ecommerceclone.Prevalent;
+
+import com.example.ecommerceclone.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
